63b71cfb0a5e05bb5a70b43b0eb666edf6222eab
Philippe H <36929410+phlhg@users.noreply.github.com>
2019-03-04 18:45:27 +0100

    Add Ability to switch view

--- app/controllers.py
+++ app/controllers.py
@@ -3,10 +3,10 @@ from .views import *
 class BaseController:
 
     def __init__(self, root):
-        BaseController.ViewManager = ViewManager(root)
+        BaseController.ViewManager = ViewManager(root, self)
 
     def view(self, name):
-        return BaseController.ViewManager.addPage(name)
+        return BaseController.ViewManager.addView(name)
 
 
 class Core(BaseController):
@@ -14,4 +14,5 @@ class Core(BaseController):
     def __init__(self, *args):
         super().__init__(*args)
         self.view("Main")
-        #self.view("Settings")
+        self.view("Settings")
+        self.view("Main")
--- app/views.py
+++ app/views.py
@@ -1,57 +1,76 @@
 from tkinter import *
 
-
 class ViewManager:
 
-    def __init__(self, root):
+    def __init__(self, root, controller):
         self.root = root
-        self.pages = []
+        self.controller = controller
 
+        self.views = []
+        self.viewsIndex = []
+
+        self.root.title("P2P Chat by Nico Widmer & Philippe Hugo")
+        self.root.iconbitmap('favicon.ico')
         self.root.geometry("920x720")
         self.root["bg"] = "#fff"
 
-    def addPage(self, name):
-        view = globals()[name+"View"](self.root, self.newFrame(name))
-        self.pages.append(view)
-        return self.pages[len(self.pages)-1]
+    def addView(self, name):
+        if name+"View" not in self.viewsIndex:
+            view = globals()[name+"View"](self.root, self.controller, self.newFrame(name))
+            self.views.append(view)
+            self.viewsIndex.append(name+"View")
+            print("NEW")
+            return self.views[len(self.views)-1]
+        else:
+            view = self.views[self.viewsIndex.index(name+"View")]
+            view.frame.tkraise()
+            return view
 
     def newFrame(self, name):
         frame = Frame(self.root, bg="#fff", height=720, width=920)
         frame.grid(row=0, column=0, sticky=N+W+S+E)
         frame.rowconfigure(0, weight=1)
         frame.columnconfigure(0, weight=1)
-
         return frame
 
 
 class BaseView:
 
-    def __init__(self, root, frame):
+    def __init__(self, root, controller, frame):
         self.root = root
+        self.controller = controller
         self.frame = frame
 
 
 class MainView(BaseView):
 
-    def __init__(self, root, frame):
-        super().__init__(root, frame)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.create()
 
     def create(self):
         sider = Frame(self.frame, bg="#eee", height=720, width=250)
         sider.grid(row=0, column=0, sticky=N+W+S)
-        header = Frame(self.frame, bg="#ddd", height=60, width=920)
+        header = Frame(self.frame, bg="#ddd", height=80, width=920)
         header.grid(row=0, column=0, sticky=N+W+E)
 
-        btn1 = Button(header, text="LOL")
+        btn1 = Button(header, bd=0 ,text="Einstellungen", command=lambda controller=self.controller: controller.view("Settings"))
         btn1.grid(row=0, column=0)
+
 
 
 class SettingsView(BaseView):
 
-    def __init__(self, root, frame):
-        super().__init__(root, frame)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.create()
 
     def create(self):
-        self.root.title("Einstellungen")
+        sider = Frame(self.frame, bg="#eee", height=720, width=250)
+        sider.grid(row=0, column=0, sticky=N+W+S)
+        header = Frame(self.frame, bg="#ddd", height=80, width=920)
+        header.grid(row=0, column=0, sticky=N+W+E)
+
+        btn1 = Button(header, bd=0, bg="transparent" text="  ❮   ", command=lambda controller=self.controller: controller.view("Main"))
+        btn1.grid(row=0, column=0)
+
