ebe7bdb3793de5b4f972aaceb6d2cdab29ce967f
MuellerNico <muellernico@outlook.com>
2019-03-25 15:03:44 +0100

    minor changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-
+from . import base
 
 connections = {}  # "ip": socket
 DEFAULT_PORT = 1516
@@ -8,6 +8,7 @@ DEFAULT_PORT = 1516
 
 class Client:
     def __init__(self):
+
         self.ip = socket.gethostbyname(socket.gethostname())
         threading.Thread(target=self.listen).start()
 
@@ -22,15 +23,19 @@ class Client:
             connections[addr] = conn
             print(connections)
 
-    def connect(ip):
+    def connect(self, ip):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, DEFAULT_PORT))
         connections[ip] = s  # add socket connection to list
+        print("connected to", s)
         s.send(b'hello there')
+
+    def connect_by_token(self, token):
+
 
     def send(self, token, text):
         if self.ip not in connections:
-            print("connection failed :/")
+            print("connection doesn't exist :/")
             return
         else:
             connections[self.ip].send(text)
