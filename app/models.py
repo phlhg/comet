ec496bce45828354ee83d706e7d103cddba735cb
MuellerNico <muellernico@outlook.com>
2019-04-02 11:24:33 +0200

    none

--- app/models.py
+++ app/models.py
@@ -59,7 +59,7 @@ class Client(BaseModel):
         self.listen()   # listen for next msg
 
     def send(self, ip, text="", command="none"):
-        print("SEND CALLED")
+        print("[log send]")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((ip, DEFAULT_PORT))
         self.contacts.getByIP(ip).createMessage(text)   # store msg for local display
